192d0c1a924646a9c8dbaf481ce36523d6d936a8
Richy <jricardo.navarros@gmail.com>
2022-02-22 18:30:10 -0600

    second commit

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+import hamburgerMenu from "./LIB/Hamburger.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", e =>{
+    hamburgerMenu(".hamburger",".contenedorMenu",".menu-item");
+})
