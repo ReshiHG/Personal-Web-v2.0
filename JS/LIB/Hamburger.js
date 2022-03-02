168f566908b31e7b66af0f94ab2353ef2cbf6bd0
Richy_wsl <jricardo.navarros@gmail.com>
2022-03-01 20:04:28 -0600

    Nav and Header with BEM methology

--- JS/LIB/Hamburger.js
+++ JS/LIB/Hamburger.js
@@ -4,11 +4,11 @@ export default function hamburgerMenu(panelBtn,panel,menuLink){
   d.addEventListener("click",e =>{
       //Revisamos que sea el objeto correcto el que origine el evento deseado
       if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
-          d.querySelector(panel).classList.toggle("menuInactivo");
+          d.querySelector(panel).classList.toggle("menu--inactivo");
           d.querySelector(panelBtn).classList.toggle("is-active");
       }
       if (e.target.matches(menuLink)){
-          d.querySelector(panel).classList.toggle("menuInactivo");
+          d.querySelector(panel).classList.toggle("menu--inactivo");
           d.querySelector(panelBtn).classList.toggle("is-active");
       }
   })
--- JS/index.js
+++ JS/index.js
@@ -3,5 +3,5 @@ import hamburgerMenu from "./LIB/Hamburger.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e =>{
-    hamburgerMenu(".hamburger",".contenedorMenu",".menu-item");
+    hamburgerMenu(".hamburger",".menu",".menu__item");
 })
