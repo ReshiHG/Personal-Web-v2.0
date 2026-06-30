c1589f6484a2d9c8cb075ea424db268d55fe83f9
RicardoNS <jricardo.navarros@gmail.com>
2026-06-30 12:41:14 -0600

    commit de prueba

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-import hamburgerMenu from "./LIB/Hamburger.js";
-
-const d = document;
-
-d.addEventListener("DOMContentLoaded", e =>{
-    hamburgerMenu(".hamburger",".contenedorMenu",".menu-item");
-})
