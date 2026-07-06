5e9183ebdc060d8d549014195986ffba899c9510
RicardoNS <jricardo.navarros@gmail.com>
2026-07-06 09:26:45 -0600

    hot fix: corrección del brakpoint entre móvil y escritorio, se omite tableta

--- src/components/common/Menu.tsx
+++ src/components/common/Menu.tsx
@@ -25,19 +25,19 @@ export function Menu({
   const handleMenuButton = () => setIsOpen(!isOpen);
   return (
     <>
-      <nav className="sticky top-0 h-16 w-full bg-jr-negro-a-900 text-white">
-        <div className="container mx-auto gap-8 md:flex md:justify-around">
-          <div className="relative z-10 flex items-center bg-jr-negro-a-900 p-3 md:flex md:gap-8">
+      <nav className="sticky top-0 h-16 w-full bg-jr-negro-a-900 text-white lg:h-auto">
+        <div className="container mx-auto gap-8 lg:flex lg:justify-around">
+          <div className="relative z-10 flex items-center bg-jr-negro-a-900 p-3 lg:flex lg:gap-8">
             <img src={logoSVG} alt={`Logo ${marcaText}`} className="h-10" />
             <h1 className="mx-auto font-Noto text-3xl font-extralight">
               {marcaText}
             </h1>
           </div>
           <div
-            className={`absolute top-0 right-0 left-0 flex h-[calc(100vh-64px)] origin-top flex-col justify-center bg-jr-negro-a-900 p-3 transition-all duration-500 ease-in-out md:static md:h-auto md:flex-row md:items-center md:gap-3 ${
+            className={`absolute top-0 right-0 left-0 flex h-[calc(100vh-64px)] origin-top flex-col justify-center bg-jr-negro-a-900 p-3 transition-all duration-500 ease-in-out lg:static lg:h-auto lg:flex-row lg:items-center lg:gap-3 ${
               isOpen
-                ? "pointer-events-auto translate-y-16 opacity-100 md:translate-y-0"
-                : "pointer-events-none -translate-y-full opacity-0 md:pointer-events-auto md:translate-y-0 md:opacity-100"
+                ? "pointer-events-auto translate-y-16 opacity-100 lg:translate-y-0"
+                : "pointer-events-none -translate-y-full opacity-0 lg:pointer-events-auto lg:translate-y-0 lg:opacity-100"
             }`}
           >
             {sections.map(({ sectionName, active }) => (
